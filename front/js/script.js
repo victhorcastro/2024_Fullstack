1d8602b1f75182ae4b52cf29ffc8d65e7b4b1f1a
Victhor Castro <victhordasvirgens@gmail.com>
2024-10-09 09:07:47 -0300

    finalizamos 2.12

--- front/js/script.js
+++ front/js/script.js
@@ -3,8 +3,17 @@ const baseURL = 'localhost:3000';
 const filmeEndPoint = '/filmes';
 
 async function obterFilmes() {
-    const URLcompleta = '${protocolo}${baseURL}${filmeEndPoint}';
+    const URLcompleta = `${protocolo}${baseURL}${filmeEndPoint}`;
     const filmes = (await axios.get(URLcompleta)).data
-
-    console.log(filmes);
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.textContent = filme.titulo
+        celulaSinopse.textContent = filme.sinopse
+        
+    }
+    //console.log(filmes);
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 //get http://localhost:3000/oi
 app.get('/oi', (req, res) => { res.send('oi') })
