740d2ff27e7b2cf4db931224288c9cf2e957eeea
Victhor Castro <victhordasvirgens@gmail.com>
2024-10-22 09:19:29 -0300

    plinio

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,42 @@
-const protocolo = 'http://';
-const baseURL = 'localhost:3000';
-const filmeEndPoint = '/filmes';
-
-async function obterFilmes() {
-    const URLcompleta = `${protocolo}${baseURL}${filmeEndPoint}`;
-    const filmes = (await axios.get(URLcompleta)).data
-    let tabela = document.querySelector('.filmes');
+const protocolo = 'http://'
+const baseURL = 'localhost:3000'
+const filmesEndpoint = '/filmes'
+function listarFilmes (filmes) {
+    let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML=""
     for (let filme of filmes) {
         let linha = corpoTabela.insertRow(0)
         let celulaTitulo = linha.insertCell(0)
         let celulaSinopse = linha.insertCell(1)
-        celulaTitulo.textContent = filme.titulo
-        celulaSinopse.textContent = filme.sinopse
-        
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
     }
-    //console.log(filmes);
 }
+async function obterFilmes () {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    listarFilmes(filmes)
+}
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    if (titulo && sinopse) {
+        tituloInput.value = ""
+        sinopseInput.value = ""
+        const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+        listarFilmes(filmes)
+    }
+    else {
+        let alert = document.querySelector('.alert')
+        alert.classList.add('show')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.remove('show')
+            alert.classList.add('d-none')
+        }, 2000)
+    }
+} 
--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-const express = require('express')
-const cors = require('cors')
+const express = require ('express')
+const cors = require ('cors')
+const mongoose = require('mongoose')
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+app.use (express.json())
+app.use (cors())
 
-//get http://localhost:3000/oi
-app.get('/oi', (req, res) => { res.send('oi') })
+const Filme = mongoose.model("Filme", mongoose.Schema({
+    titulo: {type: String},
+    sinopse: {type: String}
+}))
 
-let filmes = [
-    {
-        titulo: "Clube da Luta",
-        sinopse: "Um homem deprimido (Edward Norton) sofrendo de insônia conhece um estranho vendedor de sabão chamado Tyler Durden (Brad Pitt) e logo se vê vivendo em sua casa miserável depois que seu apartamento perfeito é destruído. Os dois homens entediados formam um clube underground com regras rígidas e lutam contra outros homens que estão fartos de suas vidas mundanas. Sua parceria perfeita se desfaz quando Marla (Helena Bonham Carter), uma colega de grupo de apoio, atrai a atenção de Tyler."
-    },
-    {
-        titulo: "Django Livre",
-        sinopse: "Django (Jamie Foxx) é um escravo liberto cujo passado brutal com seus antigos proprietários leva-o ao encontro do caçador de recompensas alemão Dr. King Schultz (Christoph Waltz). Schultz está em busca dos irmãos assassinos Brittle, e somente Django pode levá-lo a eles. O pouco ortodoxo Schultz compra Django com a promessa de libertá-lo quando tiver capturado os irmãos Brittle, vivos ou mortos."
-    }
-]
+async function connectarAoMongo() {
+    await mongoose.connect(`mongodb+srv://Banco:1234@cluster0.bd5d7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+}
 
-app.get("/filmes", (req, res) => {
+
+app.get("/filmes", async (req, res) => {
+    const filmes = await Filme.find()
     res.json(filmes)
 })
 
-app.post("/filmes", (req, res) => {
-    // captura o que o usuário enviou
+app.post("/filmes", async (req, res) => {
+    //captura o que o usuário enviou
     const titulo = req.body.titulo
     const sinopse = req.body.sinopse
-    // monta o objeto filme para incluir na base
-    const filme = {titulo: titulo, sinopse: sinopse}
-    // adiciona o novo filme a lista de filmes
-    filmes.push(filme)
-    // mostra a base atualizada
+    //montar o objeto de acordo com o modelo Filme
+    const filme = new Filme({titulo: titulo, sinopse: sinopse})
+    await filme.save()
+    const filmes = await Filme.find()
     res.json(filmes)
 })
-app.listen(3000, () => console.log("up and running"))
+
+app.listen(3000, () => {
+    try {
+        connectarAoMongo()
+        console.log("up and running")
+    } catch (e) {
+        console.log('erro na conexão', e)
+    }
+})
