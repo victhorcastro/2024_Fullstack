100848ec3955ab03c2b4cad7a0cbe2c67d0a2311
Victhor Castro <victhordasvirgens@gmail.com>
2024-10-29 09:07:56 -0300

    validação do usuario

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const cors = require ('cors')
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const app = express()
 
 app.use (express.json())
@@ -10,6 +11,13 @@ const Filme = mongoose.model("Filme", mongoose.Schema({
     titulo: {type: String},
     sinopse: {type: String}
 }))
+
+const usuarioSchema = mongoose.Schema({
+    login: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
 
 async function connectarAoMongo() {
     await mongoose.connect(`mongodb+srv://Banco:1234@cluster0.bd5d7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
@@ -31,7 +39,14 @@ app.post("/filmes", async (req, res) => {
     const filmes = await Filme.find()
     res.json(filmes)
 })
-
+app.post("/signup", async (req, res) => {
+    const login = req.body.login
+    const password = req.body.password
+    const usuario = new Usuario({login: login, password: password})
+    const respMongo = await usuario.save()
+    console.log(respMongo)
+    res.end()
+})
 app.listen(3000, () => {
     try {
         connectarAoMongo()
